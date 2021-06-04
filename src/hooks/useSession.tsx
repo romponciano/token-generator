66f0500d82e950512d895ffa82d1b5c99925af13
Rômulo Ponciano <romponciano@gmail.com>
2021-06-04 10:00:05 -0300

    fix login cache

--- src/hooks/useSession.tsx
+++ src/hooks/useSession.tsx
@@ -2,22 +2,30 @@ import { useState } from "react"
 import USER_API from "../api/users"
 
 export default function useSession() {
-    const KEY = 'session'
+    const USER_KEY = 'user'
+    const PASS_KEY = 'pass'
     
     const getSession = (): ISession => {
-        const token = sessionStorage.getItem(KEY)
-        if(token) return session    
+        const user = sessionStorage.getItem(USER_KEY)
+        const pass = sessionStorage.getItem(PASS_KEY)
+        if(user && pass) {
+            const s: ISession = {username: user, password: pass}
+            saveSession(s)
+            return s
+        }
         saveSession(undefined)
         return undefined
     }
 
     const saveSession = async (session: ISession) => {
         const token = await USER_API.login(session)
-        if(typeof token == 'string') {
-            sessionStorage.setItem(KEY, token)
+        if(typeof token == 'string' || typeof token == 'number') {
+            sessionStorage.setItem(USER_KEY, session.username)
+            sessionStorage.setItem(PASS_KEY, session.password)
             setSession(session)
         } else {
-            sessionStorage.removeItem(KEY)
+            sessionStorage.removeItem(USER_KEY)
+            sessionStorage.removeItem(PASS_KEY)
             setSession(undefined)
             throw 'Bad request'
         }
