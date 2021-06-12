82d2c5e4277fefe61c17d318b0a744fe229125af
Rômulo Ponciano <romponciano@gmail.com>
2021-06-12 01:59:15 -0300

    change notification structure

--- src/components/notification.tsx
+++ src/components/notification.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const NOTIFICATION_TYPE = {
@@ -7,14 +7,22 @@ const NOTIFICATION_TYPE = {
     WARN: 'warning'
 }
 
-const Notification: React.FC<{message: string, type: string}> = ({message, type}) => {  
+const NotificationMessage: React.FC<{
+    message: string, 
+    setMessage: (value: string) => unknown, 
+    type: string
+}> = ({message, setMessage, type}) => {  
+
     const fullClass = `notification alert alert-${type}`
 
     return(
         <>
             {message ? 
                 <StyledNotification>
-                    <div className={fullClass}><p>{message}</p></div>
+                    <div className={fullClass}>
+                        <p>{message}</p>
+                        <i className="fas fa-times" onClick={() => setMessage(undefined)} />
+                    </div>
                 </StyledNotification>
             : null }
         </>
@@ -22,15 +30,34 @@ const Notification: React.FC<{message: string, type: string}> = ({message, type}
 }
 
 export {
-    Notification,
+    NotificationMessage,
     NOTIFICATION_TYPE
 }
 
 const StyledNotification = styled.div`
     position: absolute;
-    zIndex: 500;
+    zIndex: 1000;
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
     boxShadow: 4px 4px 10px -4px rgba(0,0,0,0.58);
+
+    .alert {
+        height: 45px;
+        align-items: center;
+        margin: 0;
+        padding: 0;
+        margin: 0;
+        display: flex;
+    }
+
+    p {
+        margin: 0;
+        padding: 15px;
+    }
+
+    i {
+        cursor: pointer;
+        margin-right: 15px;
+    }
 `
